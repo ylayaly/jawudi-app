ffe2c5d02859eef408b924aa8f6df612a75f048f
ylayaly <fenix4549@hotmail.com>
2022-06-17 13:25:09 -0500

    Heading

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className='container flex flex-col mx-auto justify-center items-center p-24'>
-      <h1 className='text-red-600 font-bold text-4xl text-center'>Jawudi App</h1>
+      <h1 className='text-pink-600 font-bold text-4xl text-center'>Jawudi App</h1>
       <Link href="/home">
             <a className='mt-10 underline cursor-pointer'>Go to Home</a>
       </Link>
