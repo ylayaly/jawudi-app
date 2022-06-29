7e1ba516e4e3d5bdc317cc383168997b3897b161
ylayaly <fenix4549@hotmail.com>
2022-06-29 14:57:53 -0500

    Deploy prismic

--- pages/api/deploy.js
+++ pages/api/deploy.js
@@ -2,23 +2,18 @@ var XMLHttpRequest = require('xhr2');
 
 export default async function handler(req, res) {  
   var data = {
-    "event_type": "pismic_action"
+    "event_type": "pismic_actionnn"
   }
   var url = 'https://api.github.com/repos/ylayaly/jawudi-app/dispatches'
 
-    var xhr = new XMLHttpRequest();
-    var final_data = JSON.stringify(data)
-    
-    xhr.open('POST', url);
-    xhr.setRequestHeader('Accept', 'application/vnd.github.everest-preview+json');
-    xhr.setRequestHeader('Authorization', req.headers.authorization);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    
-    xhr.onreadystatechange = function() {
-      console.log(xhr.readyState, xhr.status)
-    }
-    
-    xhr.send(final_data)
+  var xhr = new XMLHttpRequest();
+  var final_data = JSON.stringify(data)
+  
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Accept', 'application/vnd.github.everest-preview+json');
+  xhr.setRequestHeader('Authorization', req.headers.authorization);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(final_data)
 
-  res.status(200).json({ message : "Deploly prismic" })
+  res.status(200).json({ message : "Deploy prismic", state: xhr.readyState, status : xhr.status })
 }
